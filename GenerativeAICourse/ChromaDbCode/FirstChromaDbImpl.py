533459564a2f5cf8f6724fccad167e475e4b73cf
ANTRIKSH TYAGI <antrikshtyagi@ANTRIKSHs-MacBook-Air.local>
2026-07-21 07:22:40 -0700

    latest

--- GenerativeAICourse/ChromaDbCode/FirstChromaDbImpl.py
+++ GenerativeAICourse/ChromaDbCode/FirstChromaDbImpl.py
@@ -1,13 +1,30 @@
 from langchain_openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.schema import Document
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
 load_dotenv()      #loading the environmental files
 
 
 #load the documement
 
-loder = TextLoader('bestheroes.txt')
+loder = TextLoader('/Users/antrikshtyagi/Workspace/Projects/BasicProject/GenerativeAICourse/ChromaDbCode/bestheroes.txt')
 document = loder.load();
-print(document)
+#print(document[0].page_content)
+
+#Do the text splitting
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=150,chunk_overlap=0)
+chunks = splitter.split_documents(document)
+
+
+#Create a vector store
+
+vector_store = Chroma(embedding_function=OpenAIEmbeddings(),persist_directory="/Users/antrikshtyagi/Workspace/Projects/BasicProject/GenerativeAICourse/ChromaDbCode/ChromaDatabase",collection_name="first_collection")
+#add_result =vector_store.add_documents(chunks)
+#print(add_result)
+result =vector_store.get(include=['documents','embeddings','metadatas'])
+print(result)
+
+#query_result = 
