316ca43a4578c5a537c3278504967956914f588b
ANTRIKSH TYAGI <antrikshtyagi@ANTRIKSHs-MacBook-Air.local>
2026-07-21 08:44:12 -0700

    comitn

--- GenerativeAICourse/ChromaDbCode/FirstChromaDbImpl.py
+++ GenerativeAICourse/ChromaDbCode/FirstChromaDbImpl.py
@@ -15,16 +15,18 @@ document = loder.load();
 
 #Do the text splitting
 
-splitter = RecursiveCharacterTextSplitter(chunk_size=150,chunk_overlap=0)
+splitter = RecursiveCharacterTextSplitter(chunk_size=450,chunk_overlap=0)
 chunks = splitter.split_documents(document)
 
 
 #Create a vector store
 
 vector_store = Chroma(embedding_function=OpenAIEmbeddings(),persist_directory="/Users/antrikshtyagi/Workspace/Projects/BasicProject/GenerativeAICourse/ChromaDbCode/ChromaDatabase",collection_name="first_collection")
-#add_result =vector_store.add_documents(chunks)
+add_result =vector_store.add_documents(chunks)
 #print(add_result)
 result =vector_store.get(include=['documents','embeddings','metadatas'])
-print(result)
+#print(result)
 
-#query_result = 
+query_result = "who won Dadasaheb Phalke Award"
+result_select=vector_store.similarity_search(query_result,k=2)
+print(result_select)
